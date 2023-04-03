ffb19abb76bb9660ffa3180cf1b83a1f24a630a1
Yash Surange <88226034+ysurs@users.noreply.github.com>
2023-04-03 13:39:55 +0530

    clean up

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import transformers
+from config import *
 
 # This module has been checked and is good to use
 class adapter(nn.Module):
@@ -9,12 +10,10 @@ class adapter(nn.Module):
     Adapter module implementation
     '''
     
-    def __init__(self,num_projection_nodes,num_bottleneck_nodes):
+    def __init__(self,num_feedforward_nodes):
       super().__init__()
-      self.num_projection_nodes = num_projection_nodes
-      self.num_bottleneck_nodes = num_bottleneck_nodes
-      self.linear_down_projection = nn.Linear(self.num_projection_nodes,self.num_bottleneck_nodes)
-      self.linear_up_projection = nn.Linear(self.num_bottleneck_nodes,self.num_projection_nodes)
+      self.linear_down_projection = nn.Linear(num_feedforward_nodes,bottleneck_size)
+      self.linear_up_projection = nn.Linear(bottleneck_size,num_feedforward_nodes)
       self.activation=nn.ReLU()
       
 
@@ -28,11 +27,3 @@ class adapter(nn.Module):
       return output_before_skip_connection+original_input #This acts as a skip connection 
 
 
-class adapter_bert(nn.Module):
-    
-    def __init__(self):
-        pass
-
-
-    def forward(self, input, output):
-        pass
